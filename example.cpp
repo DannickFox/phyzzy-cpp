bfea47352f83a1c21b407b4c663c7097bcabe0ea
Laura Fox <dannickfox@gmail.com>
2020-04-20 01:37:06 -0500

    changed example to box

--- example.cpp
+++ example.cpp
@@ -1,6 +1,6 @@
 // example.cpp
 /**
- * The example program sets a system of 3 masses
+ * The example program sets a system of 4 masses
  * and then simulates the movement. The magnitude
  * of the distance between the first and second
  * masses are printed into an output file named
@@ -21,11 +21,23 @@ int main (void)
     PhyzzyModel phz;
     PhyzzyEnv env(Vect2D(0, 9.81), 0);
     env.addBound(EnvBound(Vect2D(2.5, 5), Vect2D(1, 0), 0.8, 0.6, 0.9));
-    phz.addMass(20, 0.5, Vect2D(0, 0), Vect2D(0, 0));
-    phz.addMass(10, 0.5, Vect2D(1, 0), Vect2D(0, 0));
-    phz.addMass(50, 0.5, Vect2D(2, 0), Vect2D(0, 0));
-    phz.addSpring(0, 1, 0.5, 1.5, 1.1);
-    phz.addSpring(1, 2, 0.5, 0.1, 5);
+
+    Vect2D P_m0(2.01, 2);
+    Vect2D P_m1(3, 2);
+    Vect2D P_m2(3, 3);
+    Vect2D P_m3(2, 3);
+    Vect2D v0(0, 0);
+
+    phz.addMass(0.1, 0.05, P_m0, v0);
+    phz.addMass(0.1, 0.05, P_m1, v0);
+    phz.addMass(0.1, 0.05, P_m2, v0);
+    phz.addMass(0.1, 0.05, P_m3, v0);
+    phz.addSpring(0, 1, 0.8, 0.5, 1);
+    phz.addSpring(1, 2, 0.8, 0.5, 1);
+    phz.addSpring(2, 3, 0.8, 0.5, 1);
+    phz.addSpring(3, 0, 0.8, 0.5, 1);
+    phz.addSpring(0, 2, 0.8, 0.5, (P_m0 - P_m2).mag());
+    phz.addSpring(1, 3, 0.8, 0.5, (P_m1 - P_m3).mag());
 
     std::ofstream outFile("magnitudes.txt");
     for (int i = 0; i <= dataPoints; i++)
